7bec016292ed8ce4947dbfd6b4c67ae3121e8aeb
Aman Gupta <guptaaman.official@gmail.com>
2026-02-04 19:45:39 +0530

    Add LC 378 Kth Smallest Element in Sorted Matrix solution

---
+++ Searching & Sorting/KthSmallestElementInSortedMatrix.cpp
@@ -0,0 +1,50 @@
+// Problem: Kth Smallest Element in a Sorted Matrix
+// Platform: LeetCode (378)
+// Topic: Searching & Sorting
+// Technique: Binary Search on Answer / Min Heap
+// Difficulty: Medium
+
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k)
+    {
+        int n = matrix.size();
+        int low = matrix[0][0];
+        int high = matrix[n - 1][n - 1];
+        while (low < high)
+        {
+            int mid = low + (high - low) / 2;
+            int count = countLessEqual(matrix, mid);
+            if (count < k)
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid;
+            }
+        }
+        return low;
+    }
+
+    int countLessEqual(vector<vector<int>>& matrix, int mid)
+    {
+        int n = matrix.size();
+        int row = n - 1, col = 0;
+        int count = 0;
+
+        while (row >= 0 && col < n)
+        {
+            if (matrix[row][col] <= mid)
+            {
+                count += row + 1;                                                  // all elements above are <= mid
+                col++;
+            }
+            else
+            {
+                row--;
+            }
+        }
+        return count;
+    }
+};
